998f1ebd0d89edec2a9c996165ae4e9d11546307
yuralala8 <yc1435@nyu.edu>
2017-06-11 02:02:59 -0400

    simple loops commit

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,34 +9,45 @@ def loop_iterator(number_of_times)
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+  number_of_times = 0
+  loop do
+    number_of_times += 1
+      puts phrase
+     if number_of_times >= 7
+         break
+         end
+     end
 end
 
 def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    7.times do
+        puts phrase
+    end
 end
 
 def while_iterator(number_of_times)
-  # code your solution here using the "while" keyword
-  # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    number_of_times = 0
+    while number_of_times < 7
+        puts phrase
+        number_of_times += 1
+    end
 end
 
 def until_iterator(number_of_times)
-  # code your solution here using the "until" keyword
-  # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    number_of_times = 0
+    until number_of_times == 7
+        puts phrase
+    number_of_times += 1
+    end
 end
 
 def for_iterator(number_of_times)
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    number_of_times = 1..7
+    for number_of_times in 1..7 do
+        puts phrase
+    end
 end
-
